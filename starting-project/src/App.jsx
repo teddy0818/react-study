4beaa73d0bef4be112dda93f01e4a96cf17cda7a
teddy0818 <taddy0818@naver.com>
2024-01-13 07:04:55 +0900

    React 핵심 - 동적 값 출력 및 활용

--- starting-project/src/App.jsx
+++ starting-project/src/App.jsx
@@ -1,10 +1,19 @@
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
 function Header() {
+  const description = reactDescriptions[genRandomInt(2)];
+
   return (
     <header>
       <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
+        {/* 중괄호 안에 속성이나 JS 코드 작성 가능 */}
+        {description} React concepts you will need for almost any app you are
         going to build!
       </p>
     </header>
