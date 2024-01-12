fced5cf5b303b9b7bc0ea4792aaa0f3ff687e3f7
teddy0818 <taddy0818@naver.com>
2024-01-13 05:52:44 +0900

    React 핵심 - 프로젝트 생성

--- JS-Refresher/code/finished/assets/scripts/app.js
+++ JS-Refresher/code/finished/assets/scripts/app.js
@@ -5,7 +5,7 @@ import def from "./util.js";
 // 일반 방식 - 별칠 사용가능
 import { apiKey, apiKey2 as ak2 } from "./util.js";
 // 객체로 묶는 방식
-import * as util from "./util.js"
+import * as util from "./util.js";
 
 console.log(def);
 console.log(apiKey);
