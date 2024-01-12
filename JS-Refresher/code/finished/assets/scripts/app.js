d88e3a3e8cee985c67f028363d7e37da4aaf47b5
teddy0818 <taddy0818@naver.com>
2024-01-12 14:32:03 +0900

    JS 복습 - 배열 및 객체의 분해

--- JS-Refresher/code/finished/assets/scripts/app.js
+++ JS-Refresher/code/finished/assets/scripts/app.js
@@ -79,7 +79,7 @@ console.log(user1.name);
 user1.greet();
 */
 
-
+/*
 // 배열함수 - 간결한 코드 사용 가능
 const hobbies = ["Sports", "Cooking", "Reading"];
 
@@ -95,3 +95,22 @@ console.log(index);
 // map - 기존 배열을 이용해 새로운 더미값을 만들수 있음. 이때 기본배열은 안변하고 새배열 반환함
 const editedHobbies = hobbies.map((item) => ({item})); // 화살푬 함수에서 object 반환하려면 소괄호로 감싸줘야함
 console.log(editedHobbies);
+*/
+
+/*
+// 배열 및 객체의 분해
+
+// 배열 분해
+// 첫번째 상수변수에 첫번쨰 원소를 매핑하고, 두번째 상수변수에 두번째 원소를 매핑
+const [firstName, lastName] = ["Max", "Schwarzmiller"];
+console.log(firstName);
+console.log(lastName);
+
+// 객체 분해
+const { name: userName, age } = {  // 별칭 사용 가능 (userName)
+    name: "Max",
+    age: 36
+};
+console.log(userName);
+console.log(age);
+*/
