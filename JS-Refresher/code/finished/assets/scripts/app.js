b90d1b53f2153b0eee406d557dd985915eb1476f
teddy0818 <taddy0818@naver.com>
2024-01-12 10:01:28 +0900

    JS 복습 - 변수

--- JS-Refresher/code/finished/assets/scripts/app.js
+++ JS-Refresher/code/finished/assets/scripts/app.js
@@ -1,3 +1,6 @@
+/*
+// import, export 개념
+
 // default 방식 - 원하는 이름 할당
 import def from "./util.js";
 // 일반 방식 - 별칠 사용가능
@@ -9,3 +12,21 @@ console.log(def);
 console.log(apiKey);
 console.log(ak2);
 console.log(util.apiKey);
+
+*/
+
+/*
+
+// 변수   
+// JS에선 관용적으로 camelCase 방식으로 변수명을 작성
+// 일반적으로 let 을 사용
+let userMessage = "Hello World!";
+console.log(userMessage);
+// const - 상수, 상수엔은 값을 다시 할당할 수 없음
+const userMessage2 = "Hello World 2";
+console.log(userMessage2);
+
+*/
+
+// 연산자
+
