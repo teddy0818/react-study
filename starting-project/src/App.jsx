0bc657b90de2a67655d133e959962df30ca74f68
teddy0818 <taddy0818@naver.com>
2024-01-13 14:53:40 +0900

    React 핵심 - 함수를 props 값으로 전달

--- starting-project/src/App.jsx
+++ starting-project/src/App.jsx
@@ -3,6 +3,10 @@ import Header from "./components/Header/Header.jsx";
 import CoreConcept from "./components/CoreConcept.jsx";
 import TabButton from "./components/TabButton.jsx";
 
+function handleClick() {
+  console.log("Click !!");
+}
+
 function App() {
   return (
     <div>
@@ -20,10 +24,10 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton>Components</TabButton>
-            <TabButton>JSX</TabButton>
-            <TabButton>Props</TabButton>
-            <TabButton>State</TabButton>
+            <TabButton onSelect={handleClick}>Components</TabButton>
+            <TabButton onSelect={handleClick}>JSX</TabButton>
+            <TabButton onSelect={handleClick}>Props</TabButton>
+            <TabButton onSelect={handleClick}>State</TabButton>
           </menu>
         </section>
         <h2>Time to get started!</h2>
--- starting-project/src/components/TabButton.jsx
+++ starting-project/src/components/TabButton.jsx
@@ -1,7 +1,13 @@
-export default function TabButton(props) {
+export default function TabButton({ children, onSelect }) {
+  {
+    /* 매개변수명은 사용자 마음대로 지어도 됨(children는 특수한 경우) */
+    /* 보통 함수경우 on 으로 시작하는 경우가 많음 */
+    /* props 에서 쓰일 함수명 - handle로 시작하는 경우가 많음 */
+  }
+
   return (
     <li>
-      <button>{props.children}</button>
+      <button onClick={onSelect}>{children}</button>
     </li>
   );
 }
