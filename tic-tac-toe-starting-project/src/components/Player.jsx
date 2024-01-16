932530fb2310f0b2226027ecf7addea2bc053ffa
teddy0818 <taddy0818@naver.com>
2024-01-16 19:14:29 +0900

    틱택토 - 상태값 변경시 주의사항

--- tic-tac-toe-starting-project/src/components/Player.jsx
+++ tic-tac-toe-starting-project/src/components/Player.jsx
@@ -4,12 +4,19 @@ export default function Player({ name, symbol }) {
   const [isEditing, setIsEditing] = useState(false);
 
   const handleEditClick = () => {
-    setIsEditing(!isEditing);
+    // 1. 잘못 된 오래 된 방식 (전의 상태값을 변경시)
+    // setIsEditing(!isEditing); // true
+    // setIsEditing(!isEditing); // true
+
+    // 2. 옳은 방식
+    setIsEditing((editing) => !editing);
+    setIsEditing((editing) => !editing);
   };
 
   let playerName = <span className="player-name">{name}</span>;
+
   if (isEditing) {
-    playerName = <input type="text" value={name} required />;
+    playerName = <input type="text" required value={name} />;
   }
 
   return (
@@ -17,7 +24,7 @@ export default function Player({ name, symbol }) {
       <span className="player">
         {playerName}
         <span id="player-symbol">{symbol}</span>
-        <button onClick={handleEditClick}>Edit</button>
+        <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
       </span>
     </li>
   );
