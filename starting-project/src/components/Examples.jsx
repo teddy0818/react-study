65bb6f1d3f4b714b06c5972e66a24d43a308c4eb
teddy0818 <taddy0818@naver.com>
2024-01-16 17:00:59 +0900

    React 심화 - Props 기본값 설정

--- starting-project/src/components/Examples.jsx
+++ starting-project/src/components/Examples.jsx
@@ -31,7 +31,6 @@ export default function Examples() {
   return (
     <Section title="Examples" id="examples">
       <Tabs
-        ButtonsContainer="menu"
         buttons={
           <>
             <TabButton
