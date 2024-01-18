bb65c3d9d238055edf0afcddf9d178bd76741c6e
teddy0818 <taddy0818@naver.com>
2024-01-18 19:02:56 +0900

    투자계산기 - 입력값 state 생성

--- investment_calculator/src/App.jsx
+++ investment_calculator/src/App.jsx
@@ -1,12 +1,30 @@
+import { useState } from "react";
 import Header from "./components/Header";
 import UserInputs from "./components/UserInputs";
 import Result from "./components/Result";
 
+const INITIAL_INPUTS = {
+  initialInvestment: 0,
+  annualInvestment: 0,
+  expectedReturn: 0,
+  duration: 0,
+};
+
 function App() {
+  const [inputs, setInputs] = useState(INITIAL_INPUTS);
+
+  const handleChangeInput = (name, value) => {
+    setInputs((prevInputValues) => ({
+      ...prevInputValues,
+      [name]: value,
+    }));
+    console.log(inputs);
+  };
+
   return (
     <>
       <Header />
-      <UserInputs />
+      <UserInputs paramInputs={inputs} onInput={handleChangeInput} />
       <Result />
     </>
   );
--- investment_calculator/src/components/UserInputs.jsx
+++ investment_calculator/src/components/UserInputs.jsx
@@ -1,25 +1,19 @@
-export default function UserInputs() {
+export default function UserInputs({ paramInputs, onInput }) {
   return (
     <div id="user-input">
       <div className="input-group">
-        <div>
-          <label>Investment Calculator</label>
-          <input type="text" />
-        </div>
-        <div>
-          <label>Investment Calculator</label>
-          <input type="text" />
-        </div>
-      </div>
-      <div className="input-group">
-        <div>
-          <label>Investment Calculator</label>
-          <input type="text" />
-        </div>
-        <div>
-          <label>Investment Calculator</label>
-          <input type="text" />
-        </div>
+        {paramInputs &&
+          Object.entries(paramInputs).map(([name, value]) => (
+            <div>
+              <label>{name}</label>
+              <input
+                key={name}
+                type="number"
+                value={value}
+                onChange={(e) => onInput(name, e.target.value)}
+              />
+            </div>
+          ))}
       </div>
     </div>
   );
