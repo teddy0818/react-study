5ac63e65317525e2ca81a33b4b6cbb332d6188d0
teddy0818 <taddy0818@naver.com>
2024-01-20 18:31:29 +0900

    스타일링 - Tailwind CSS  세팅

--- stylingReact-project/tailwind.config.js
+++ stylingReact-project/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./src/**/*.{html,js}"],
+module.exports = {
+  content: ["./src/**/*.{html,js,ts,jsx,tsx}", "./index.html"],
   theme: {
     extend: {},
   },
