1e51b19edfa2e4e19c3ef47de1f366c7b53d4a1f
teddy0818 <taddy0818@naver.com>
2024-01-20 18:49:16 +0900

    스타일링 - Tailwind CSS - header 미디어쿼리 추가

--- stylingReact-project/src/components/Header.jsx
+++ stylingReact-project/src/components/Header.jsx
@@ -2,13 +2,13 @@ import logo from "../assets/logo.png";
 
 export default function Header() {
   return (
-    <header className="flex flex-col items-center mt-8 mb-16">
+    <header className="flex flex-col items-center mt-8  mb-8 md:mb-16">
       <img
         src={logo}
         alt="A canvas"
         className="object-contain mb-8 w-44 h-44"
       />
-      <h1 className="font-semibold tracking-widest text-center uppercase text-amber-900 text-4xl font-title">
+      <h1 className="font-semibold tracking-widest text-center uppercase text-amber-900 text-xl md:text-4xl font-title">
         ReactArt
       </h1>
       <p>A community of artists and art-lovers.</p>
