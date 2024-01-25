b61b66b755866eab17c968e62a130d647f81fef8
teddy0818 <taddy0818@naver.com>
2024-01-25 19:41:39 +0900

    참조 - 타이머 설정

--- refsAndPortals-project/src/components/TimerChallenge.jsx
+++ refsAndPortals-project/src/components/TimerChallenge.jsx
@@ -1,14 +1,33 @@
+import React, { useState } from "react";
+
 export default function PlayTimerChallenge({ title, targetTime }) {
+  const [timerExpired, setTimerExpired] = useState();
+  const [timerStarted, setTimerStarted] = useState();
+
+  function handleStart() {
+    setTimeout(() => {
+      setTimerExpired(true);
+    }, targetTime * 1000);
+    setTimerStarted(true);
+  }
+
+  function handleStop() {}
+
   return (
     <section className="challenge">
       <h2>{title}</h2>
+      {timerExpired && <p>You lost!</p>}
       <p className="chaalenge-time">
         {targetTime} second{targetTime > 1 ? "s" : ""}
       </p>
       <p>
-        <button>Start Challenge</button>
+        <button onClick={handleStart}>
+          {timerStarted ? "Stop" : "Start"} Challenge
+        </button>
       </p>
-      <p>Time is running... / Timer inactive</p>
+      <p className={timerStarted ? "active" : undefined}>
+        {timerStarted ? "Time is running..." : "Timer inactive"}
+      </p>
     </section>
   );
 }
