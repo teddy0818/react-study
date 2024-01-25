1a7d4e1e1df2429262209c161b547a80cf8f4092
teddy0818 <taddy0818@naver.com>
2024-01-25 20:01:50 +0900

    참조 - 참조의 활용

--- refsAndPortals-project/src/components/TimerChallenge.jsx
+++ refsAndPortals-project/src/components/TimerChallenge.jsx
@@ -1,17 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
+
+// 1. 전역변수로 해야 timer 함수가 컴포넌트 재실행에 방해받지 않는다
+// 2. 컴포넌트 별로 timer 변수를 공유하기 때문에 로직에 이상이 생간다 - timer가 초기화 되버린다
+// 3. 참조가 답이다 !!
+// let timer;
 
 export default function PlayTimerChallenge({ title, targetTime }) {
+  // 컴포넌트 내 다른 인스턴스들의 참조가 만들어져 독립적으로 동작한다
+  // 컴포넌트가 재실행되도 참조값은 유실되지 않는다
+  // timer 자체가 UI와 직접적인 영향이 없기 때문에 사용하기 좋은 사례다
+  const timer = useRef();
   const [timerExpired, setTimerExpired] = useState();
   const [timerStarted, setTimerStarted] = useState();
 
   function handleStart() {
-    setTimeout(() => {
+    timer.current = setTimeout(() => {
       setTimerExpired(true);
     }, targetTime * 1000);
     setTimerStarted(true);
   }
 
-  function handleStop() {}
+  function handleStop() {
+    clearInterval(timer.current);
+  }
 
   return (
     <section className="challenge">
@@ -21,7 +32,7 @@ export default function PlayTimerChallenge({ title, targetTime }) {
         {targetTime} second{targetTime > 1 ? "s" : ""}
       </p>
       <p>
-        <button onClick={handleStart}>
+        <button onClick={timerStarted ? handleStop : handleStart}>
           {timerStarted ? "Stop" : "Start"} Challenge
         </button>
       </p>
