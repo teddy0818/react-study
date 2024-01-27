804deb10a9be96aa00a7e61c9d4b3746c9768ec6
teddy0818 <taddy0818@naver.com>
2024-01-27 19:23:04 +0900

    참조 - 모달창 렌더링 위치 변경 (Portals 활용)

--- refsAndPortals-project/src/components/ResultModal.jsx
+++ refsAndPortals-project/src/components/ResultModal.jsx
@@ -1,4 +1,5 @@
 import React, { forwardRef, useImperativeHandle, useRef, onReset } from "react";
+import { createPortal } from "react-dom";
 
 const ResultModal = forwardRef(function ResultModal(
   { targetTime, remainingTime },
@@ -21,7 +22,7 @@ const ResultModal = forwardRef(function ResultModal(
     };
   });
 
-  return (
+  return createPortal(
     <dialog ref={dialog} className="result-modal" onClose={onReset}>
       {userLost && <h2>You Lost</h2>}
       {!userLost && <h2>Your Score: {score}</h2>}
@@ -35,7 +36,8 @@ const ResultModal = forwardRef(function ResultModal(
       <form method="dialog" onSubmit={onReset}>
         <button>Close</button>
       </form>
-    </dialog>
+    </dialog>,
+    document.getElementById("modal")
   );
 });
 
