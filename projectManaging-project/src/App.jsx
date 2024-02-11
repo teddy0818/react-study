8d7720ed8a0187a6b341e072848aa0eb4c828c79
teddy0818 <taddy0818@naver.com>
2024-02-11 11:16:05 +0900

    프로젝트 매니징 - 프로젝트 사이드바 목록

--- projectManaging-project/src/App.jsx
+++ projectManaging-project/src/App.jsx
@@ -21,20 +21,19 @@ function App() {
 
   function handleAddProject(projectData) {
     setProjectsState((prevState) => {
+      const projectId = Math.random();
       const newProject = {
         ...projectData,
-        id: Math.random(),
+        id: projectId,
       };
 
       return {
         ...prevState,
+        selectedProjectId: undefined,
         projects: [...prevState.projects, newProject],
-        selectedProjectId: newProject.id,
       };
     });
   }
-
-  console.log(projectsState.projects);
 
   let content;
 
@@ -46,7 +45,10 @@ function App() {
 
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectSidebar onStartAddProject={handleStartAddProject} />
+      <ProjectSidebar
+        onStartAddProject={handleStartAddProject}
+        projects={projectsState.projects}
+      />
       {content}
     </main>
   );
