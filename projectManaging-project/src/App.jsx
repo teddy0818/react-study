6019aac6a90c9deebd9cb2dd8bd26608be0b2f74
teddy0818 <taddy0818@naver.com>
2024-02-11 10:21:52 +0900

    프로젝트 매니징 - projectsState  및 관련 로직 추가

--- projectManaging-project/src/App.jsx
+++ projectManaging-project/src/App.jsx
@@ -1,13 +1,36 @@
+import { useState } from "react";
+
 import NewProject from "./components/NewProject.jsx";
 import ProjectSidebar from "./components/ProjectSidebar.jsx";
 import NoProjectSelected from "./components/NoProjectSelected.jsx";
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined, // undefined: 프로젝트를 선택하지 않음, null : 새로운 프로젝트 추가
+    projects: [],
+  });
+
+  function handleStartAddProject() {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  }
+
+  let content;
+
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject />;
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectSidebar />
-      <NoProjectSelected />
-      {/* <NewProject /> */}
+      <ProjectSidebar onStartAddProject={handleStartAddProject} />
+      {content}
     </main>
   );
 }
