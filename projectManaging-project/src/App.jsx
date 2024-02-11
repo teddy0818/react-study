255463e91814fe008390f43468bc56ad2d0bafa9
teddy0818 <taddy0818@naver.com>
2024-02-11 09:51:42 +0900

    프로젝트 매니징 - Button, NoProjectSelected 컴포넌트 생성

--- projectManaging-project/src/App.jsx
+++ projectManaging-project/src/App.jsx
@@ -1,11 +1,13 @@
 import NewProject from "./components/NewProject.jsx";
 import ProjectSidebar from "./components/ProjectSidebar.jsx";
+import NoProjectSelected from "./components/NoProjectSelected.jsx";
 
 function App() {
   return (
     <main className="h-screen my-8 flex gap-8">
       <ProjectSidebar />
-      <NewProject />
+      <NoProjectSelected />
+      {/* <NewProject /> */}
     </main>
   );
 }
